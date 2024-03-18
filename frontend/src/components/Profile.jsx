8993a8495f1544c38f59df4a9a1f0062fa01419d
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-18 14:12:23 +0530

    added profile image in profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,7 +1,17 @@
 function Profile(){
     return(
         <>
-        <h1>Profile</h1>
+        <div class= "flex items-center justify-center">
+        <div class="bg-white w-1/3 mt-10 rounded-lg">
+          <dev className="flex items-center justify-center">
+          <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  class="rounded-full w-32"
+                />
+          </dev>
+        </div>
+        </div>
         </>
     )
 }
