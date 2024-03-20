eac1c74b8ac526aaf133a0e4a88ae12d74634d99
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-20 08:48:22 +0530

    add Pdf to vectorDB function added

--- RAG/RAG.py
+++ RAG/RAG.py
@@ -13,11 +13,6 @@ import torch
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, ContentSettings
 device = torch.device('cuda'if torch.cuda.is_available() else'cpu')
 
-def getEmbeddings(text,model_name='all-MiniLM-L6-v2'):
-    model = SentenceTransformer(model_name)
-    embeddings = model.encode(text)
-    return embeddings
-
 class Document:
   def __init__(self, page_content,embedding,  metadata,id,user_id):
       self.page_content = page_content
@@ -25,6 +20,12 @@ class Document:
       self.metadata = metadata
       self.id = id
       self.user_id = user_id
+
+def getEmbeddings(text,model_name='all-MiniLM-L6-v2'):
+    model = SentenceTransformer(model_name)
+    embeddings = model.encode(text)
+    return embeddings
+
 
 # Function to embed and store chunks in vector database
 def embed_and_store_chunks(folder_path):
@@ -117,6 +118,39 @@ def download_folder_from_blob(account_name, account_key, container_name, remote_
      blob_client = container_client.get_blob_client(blob_name)
      with open(local_file_path, "wb") as data:
          data.write(blob_client.download_blob().readall())
+
+def addPDFtoVectorDB(filepath,vectorDBpath,model='all-MiniLM-L6-v2' ):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    doc_chunks = []
+    download_folder_from_blob(account_name, account_key, container_name, "faiss_index", vectorDBpath)
+    vector_db = FAISS.load_local(vectorDBpath,model)
+    if filepath.endswith('pdf'):
+      filename = os.path.basename(filepath)
+      print(filename,"started")
+      elements = partition(filepath)
+      # try:
+      #   elements = partition(filepath)
+      # except Exception as e:
+      #   print(f"An error occurred when trying to partition the file: {e,filename}")
+      chunks = chunk_by_title(elements, new_after_n_chars=1500, combine_text_under_n_chars=700)
+      for i, chunk in enumerate(chunks):
+            # Embed the chunk using SentenceTransformer
+            chunk_embedding = model.encode(chunk.text)
+            page_number = chunk.metadata.page_number
+            doc_id = f"{filename}-{i}"
+            # Create a Document object for the chunk
+            doc = Document(page_content=chunk.text, embedding=chunk_embedding ,metadata={"page": page_number},id=doc_id)
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc_id}"
+            doc.metadata["filename"] = filename
+            doc_chunks.append(doc)
+      print(filename,"complete")
+      metadatax = [doc.metadata for doc in doc_chunks]  # Extract metadata from each Document
+      idx = [doc.id for doc in doc_chunks]  # Extract ID from each Document
+      # Extract just the embeddings from each Document
+      embeddings = [model.encode(doc.page_content) for doc in doc_chunks]
+      # Add the embeddings to the vectorstore
+      vector_db.add_embeddings(list(zip([doc.page_content for doc in doc_chunks], embeddings)), metadatas=metadatax, ids=idx)
+      upload_folder_to_blob(account_name, account_key, container_name, vectorDBpath, "faiss_index")
 
 
 if __name__ == '__main__':
