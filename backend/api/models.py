852c9f9780c50854fa5aedecce9ba3a29ec6c715
Nazal-nihad <nazalnihad17@gmail.com>
2024-03-31 23:58:10 +0530

    subject table and notes table creation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
-
-<<<<<<< HEAD
+from django.contrib.auth.models import User
 # Create your models here.
 # class UserData(models.Model):
 #     user_name = models.CharField(max_length=100)
@@ -11,22 +10,11 @@ from django.contrib.auth.hashers import make_password, check_password
 
 #     def __str__(self) -> str:
 #       return f"{self.user_name} , {self.user_id} , {self.email}"
-=======
-class UserData(models.Model):
-    user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=128)  # Increase the max_length to accommodate hashed passwords
-    user_id = models.AutoField(primary_key=True)  
-    email = models.EmailField(unique=True)  
+class Notes(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextFielde()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
 
-    def save(self, *args, **kwargs):
-        # Hash the password before saving
-        self.password = make_password(self.password)
-        super().save(*args, **kwargs)
-
-    def check_password(self, raw_password):
-        # Check if the provided password matches the hashed password
-        return check_password(raw_password, self.password)
-
-    def __str__(self) -> str:
-        return f"{self.user_name} , {self.user_id} , {self.email}"
->>>>>>> 33d12a0bd9b9d19857b4f2576f4a3122544e1f36
+    def __str__(self):
+        return self.title
