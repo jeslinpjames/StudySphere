8f5af4b763b71d7fffa2f582ad59d39e714c6ab8
Nazal-nihad <nazalnihad17@gmail.com>
2024-04-01 02:05:03 +0530

    subject creation and deletion api setup although not complete , adding their paths to url

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated,AllowAny
 # Create your views here.
 
-    
+    # user api view
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-
+# notes creation with authentication only
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -27,6 +27,7 @@ class NoteListCreate(generics.ListCreateAPIView):
         else:
             print(serializer.errors)
 
+# note deletion
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -34,3 +35,28 @@ class NoteDelete(generics.DestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Notes.objects.filter(author = user)
+
+# subject creation api
+class SubjectListCreate(generics.ListCreateAPIView):
+    serializer_class = SubjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Subjects.objects.filter(author = user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            # must add field to get subject from user and save it here 
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+# subject deleteion api
+class SubjectDelete(generics.DestroyAPIView):
+    serializer_class = SubjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Subjects.objects.filter(author = user)
