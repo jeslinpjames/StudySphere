9f1b683f762b0cf82feef8b4c8afcb709ac51b0d
Nazal Nihad <118171350+nazalnihad@users.noreply.github.com>
2024-04-17 11:10:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,116 +22,142 @@ import Question from "./components/Quiz/Question";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import NotFound from "./components/NotFound";
 
-function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />;
-}
+// function RegisterAndLogout() {
+//   localStorage.clear();
+//   return <Register />;
+// }
 
 function App() {
-  const location = useLocation();
-  const accessToken = localStorage.getItem("access");
-  // const refreshToken = localStorage.getItem("refreshToken");
+//   const location = useLocation();
+//   const accessToken = localStorage.getItem("access");
+//   // const refreshToken = localStorage.getItem("refreshToken");
 
-  // Check if both tokens exist and are not empty strings
-  const isAuthenticated = accessToken;
+//   // Check if both tokens exist and are not empty strings
+//   const isAuthenticated = accessToken;
+
+//   return (
+//     <>
+//       {/* Dashboard visibility */}
+//       {isAuthenticated &&
+//         !(
+//           location.pathname === "/login" || location.pathname === "/register"
+//         ) && <DashBoard />}
+//       <Routes>
+//         {/* Public Routes */}
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<RegisterAndLogout />} />
+//         <Route path="*" element={<NotFound />} />
+
+//         {/* Protected Routes */}
+//         <Route
+//           path="/"
+//           element={<ProtectedRoutes>{/* <DashBoard /> */}</ProtectedRoutes>}
+//         />
+//         <Route
+//           path="/home"
+//           element={<ProtectedRoutes>{/* <DashBoard /> */}</ProtectedRoutes>}
+//         />
+//         <Route
+//           path="/flashcards"
+//           element={
+//             <ProtectedRoutes>
+//               <FlashCard />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/quiz"
+//           element={
+//             <ProtectedRoutes>
+//               <Quiz />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/notes"
+//           element={
+//             <ProtectedRoutes>
+//               <Notes />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/notes/:subject"
+//           element={
+//             <ProtectedRoutes>
+//               <NotesList />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/chatpdf"
+//           element={
+//             <ProtectedRoutes>
+//               <ChatPdf />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/profile"
+//           element={
+//             <ProtectedRoutes>
+//               <Profile />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/flashcards/:subject"
+//           element={
+//             <ProtectedRoutes>
+//               <FlashCardList />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/quiz/:subject"
+//           element={
+//             <ProtectedRoutes>
+//               <QuestionsList />
+//             </ProtectedRoutes>
+//           }
+//         />
+//         <Route
+//           path="/quiz/:subject/:qn_id"
+//           element={
+//             <ProtectedRoutes>
+//               <Question />
+//             </ProtectedRoutes>
+//           }
+//         />
+//       </Routes>
+//     </>
+//   );
+const location = useLocation();
 
   return (
     <>
       {/* Dashboard visibility */}
-      {isAuthenticated &&
-        !(
-          location.pathname === "/login" || location.pathname === "/register"
-        ) && <DashBoard />}
+      {!(
+        location.pathname === "/login" || location.pathname === "/register"
+      ) && <DashBoard />}
       <Routes>
-        {/* Public Routes */}
+        {/* Routes */}
+        <Route path="/flashcards" element={<FlashCard />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/notes/:subject" element={<NotesList />} />
+        <Route path="/chatpdf" element={<ChatPdf />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />} />
-
-        {/* Protected Routes */}
-        <Route
-          path="/"
-          element={<ProtectedRoutes>{/* <DashBoard /> */}</ProtectedRoutes>}
-        />
-        <Route
-          path="/home"
-          element={<ProtectedRoutes>{/* <DashBoard /> */}</ProtectedRoutes>}
-        />
-        <Route
-          path="/flashcards"
-          element={
-            <ProtectedRoutes>
-              <FlashCard />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/quiz"
-          element={
-            <ProtectedRoutes>
-              <Quiz />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/notes"
-          element={
-            <ProtectedRoutes>
-              <Notes />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/notes/:subject"
-          element={
-            <ProtectedRoutes>
-              <NotesList />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/chatpdf"
-          element={
-            <ProtectedRoutes>
-              <ChatPdf />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoutes>
-              <Profile />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/flashcards/:subject"
-          element={
-            <ProtectedRoutes>
-              <FlashCardList />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/quiz/:subject"
-          element={
-            <ProtectedRoutes>
-              <QuestionsList />
-            </ProtectedRoutes>
-          }
-        />
-        <Route
-          path="/quiz/:subject/:qn_id"
-          element={
-            <ProtectedRoutes>
-              <Question />
-            </ProtectedRoutes>
-          }
-        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/flashcards/:subject" element={<FlashCardList />} />
+        <Route path="/quiz/:subject" element={<QuestionsList />} />
+        <Route path="/quiz/:subject/:qn_id" element={<Question />} />
       </Routes>
     </>
   );
+
 }
 
+  
 export default App;
